8d3654ede3be3cc7b3f410bbb285cf8cc719f9e3
Ravaka82 <89504880+Ravaka82@users.noreply.github.com>
2025-03-05 19:19:27 +0300

    initial react-jsonschema-form

---
+++ only-hunters/src/pages/_app.tsx
@@ -0,0 +1,11 @@
+// pages/_app.tsx
+
+import "@/styles/globals.css";
+import FormPage from "./MultiStepForm"; 
+export default function App() {
+  return (
+    <div>
+      <FormPage /> 
+    </div>
+  );
+}
---
+++ only-hunters/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500 text-white">
+      <h1 className="text-3xl font-bold">Hello, Tailwind fonctionne ! 🎉</h1>
+    </div>
+  );
+}
